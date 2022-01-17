bcd93b820c3602030845212b7155da3699f489ad
braveoni <49433877+braveoni@users.noreply.github.com>
2022-01-18 00:50:26 +0300

    Import Bootstrap

--- web/__init__.py
+++ web/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+
 
 app = Flask(__name__)
 app.config.from_object('web.config.Config')
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
+bootstrap = Bootstrap(app)
+
 
 from web import routes
