e076b33aab900f39d34298c0e32d854ed6f55a69
braveoni <49433877+braveoni@users.noreply.github.com>
2022-01-17 23:37:25 +0300

    Create function for login check

---
+++ web/utils.py
@@ -0,0 +1,12 @@
+from .models import Users
+from werkzeug.security import check_password_hash
+
+
+def check_login(username: str, password: str) -> bool | Users:
+    if username and password:
+        user = Users.query.filter_by(username=username).first()
+
+        if user and check_password_hash(user.password, password):
+            return user
+
+    return False
