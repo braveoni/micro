25c352393459fcf40df2affdfc7d365e3b70389f
braveoni <49433877+braveoni@users.noreply.github.com>
2022-01-17 23:22:57 +0300

    Refactoring

--- web/__init__.py
+++ web/__init__.py
@@ -1,11 +1,10 @@
 from flask import Flask
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('web.config.Config')
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
 
-
-@app.route('/')
-def index():
-    return {'hell': 'no'}
+from web import routes
