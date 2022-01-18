7dbe5cc83365db29a440204ace37735b3bf2d174
braveoni <49433877+braveoni@users.noreply.github.com>
2022-01-18 06:44:14 +0300

    Add create user route

--- web/routes.py
+++ web/routes.py
@@ -1,7 +1,11 @@
 from flask import request, redirect, flash, render_template, url_for
 from flask_login import login_user, login_required, current_user, logout_user
 from . import app
-from .utils import check_login
+from .forms import UserForm
+from .manager import UserManager, DoorManager
+
+u_manager = UserManager()
+d_manager = DoorManager()
 
 
 @app.route('/main', methods=['GET'])
@@ -12,17 +16,18 @@ def main():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    username = request.form.get('username')
-    password = request.form.get('password')
+    form = UserForm()
 
-    if user := check_login(username, password):
-        login_user(user)
-        next_page = request.args.get('next')
-        return redirect(next_page)
-    else:
-        flash('Incorrect login or password')
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            username, password = form.data['username'], form.data['password']
+            if user := u_manager.check(username, password):
+                login_user(user)
+                return redirect(url_for('main'))
+            else:
+                flash('Incorrect login or password')
 
-    return render_template('login.html')
+    return render_template('login.html', form=form)
 
 
 @app.route('/logout', methods=['GET', 'POST'])
@@ -32,9 +37,26 @@ def logout():
     return redirect(url_for('login'))
 
 
+@app.route('/users', methods=['GET', 'POST'])
+@login_required
+def create_user():
+    form = UserForm()
+
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            username, password = form.data['username'], form.data['password']
+            if u_manager.get(username=username):
+                flash('Such user already exist')
+            else:
+                u_manager.add(username, password)
+                return redirect(url_for('create_user'))
+
+    return render_template('create_user.html', form=form)
+
+
 @app.after_request
 def redirect_to_login(response):
     if response.status_code == 401:
-        return redirect(url_for('login') + '?next=' + request.url)
+        return redirect(url_for('login'))
 
     return response
